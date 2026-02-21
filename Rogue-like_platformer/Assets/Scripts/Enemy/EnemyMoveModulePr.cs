47e45bc87bfec42343bb244ef46941cfb47e981c
lllAboballl <shkarupa.avn@gmail.com>
2026-02-21 14:13:15 +0100

    1

---
+++ Rogue-like_platformer/Assets/Scripts/Enemy/EnemyMoveModulePr.cs
@@ -0,0 +1,30 @@
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class EnemyMoveModulePr : EnemyModule
+{
+
+    private Rigidbody2D rb;
+    LayerMask layerMask;
+    protected override void Awake()
+    {
+        base.Awake();
+        rb = GetComponent<Rigidbody2D>();
+        layerMask = LayerMask.GetMask("Wall");
+    }
+
+    public override void Tick()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.forward), Mathf.Infinity, layerMask);
+        if (hit.collider != null)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+         else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
