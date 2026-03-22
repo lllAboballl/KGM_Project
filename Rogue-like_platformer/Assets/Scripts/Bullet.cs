3b51236fa1b23289aaaee72d68effe2212e8b1db
Liam-Bartmar-Klaving <liam.bartmarklaving@gmail.com>
2026-03-22 18:21:28 +0100

    Bullets get destroyed on collision

---
+++ Rogue-like_platformer/Assets/Scripts/Bullet.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) return;
+        else Destroy(gameObject);
+    }
+}
