55e0166b9fb7ef149fadb05c6095017f9318202b
Josef-Vimar <vimarolsson@gmail.com>
2026-03-26 20:50:51 +0100

    Added XP systrem + XP bar
    (No level up or upgrades yet)

---
+++ Rogue-like_platformer/Assets/Scripts/ExperienceManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ExperienceManager : MonoBehaviour
+{
+    [SerializeField] int totalXp = 1;
+
+    [SerializeField] int currentLevel = 0;
+
+    [SerializeField] Image fillimage;
+
+    [SerializeField] TextMeshProUGUI levelText;
+
+    void Start()
+    {
+        UpdateXpBar();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    int LevelCalculator(int xp)
+    {
+        int calculatedLevel = 0;
+        while (xp > 0)
+        {
+            xp -= ((int)Mathf.Pow(calculatedLevel, 2.25f) + 2);
+            calculatedLevel++;
+     
+        }
+        
+        return calculatedLevel;
+        
+    }
+
+    int GetXpRequiredForLevel(int levelToGet)
+    {
+        int requiredXp = 0;
+        while(LevelCalculator(requiredXp) < levelToGet)
+        {
+            requiredXp++;
+         
+        }
+
+        return requiredXp;
+    }
+
+    void UpdateXpBar()
+    {
+        currentLevel = LevelCalculator(totalXp);
+        levelText.text = currentLevel.ToString();
+
+        int xpRequiredForNextLevel = GetXpRequiredForLevel(currentLevel + 1);
+        int xpRequiredForCurrentLevel = GetXpRequiredForLevel(currentLevel);
+
+        float xpBarPercentage = ((float)xpRequiredForNextLevel -
+            (float)xpRequiredForCurrentLevel) /
+            (float)xpRequiredForNextLevel;
+       
+        fillimage.fillAmount = xpBarPercentage;
+    }
+
+}
+
